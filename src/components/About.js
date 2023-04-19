7c5392c205a72f73501cbaec131b67958d9210f3
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-04-19 14:17:50 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { image } from "../data/user";
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src="https://images.unsplash.com/photo-1635863138275-d9b33299680b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aXJvbiUyMG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="I made this" />
     </div>
   );
 }
+
+export default About;
